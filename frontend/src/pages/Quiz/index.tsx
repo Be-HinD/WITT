db8b66b7d0acd9f79526a7c8c97e75537c3fe81f
JIYEON KIM <jiyeon2536@naver.com>
2024-03-21 17:24:31 +0900

    [FE] fix #S10P22D103-83 카메라 전면 수정

--- frontend/src/pages/Quiz/index.tsx
+++ frontend/src/pages/Quiz/index.tsx
@@ -1,11 +1,23 @@
 import { Link } from 'react-router-dom'
-
+import { useState } from 'react'
 const Quiz = () => {
+	const [capturedImage, setCapturedImage] = useState<File | undefined>(undefined)
+	const handleCaptureImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const file = e.target.files?.[0]
+		if (file) {
+			setCapturedImage(file)
+		}
+	}
 	return (
 		<div>
 			<p>Quiz Page</p>
 			<p>사용방법</p>
-			<p>사진 찍고 퀴즈 풀기</p>
+			<p>
+				<input type="file" accept="image/*" capture="environment" onChange={handleCaptureImage}>
+					사진 찍고 퀴즈 풀기
+				</input>
+				{capturedImage ? <img src={URL.createObjectURL(capturedImage)} /> : <div>사진 없음</div>}
+			</p>
 			<Link
 				to="/quiz/camera"
 				className="bg-green-700 text-white rounded-full px-2 py-1 leading-9 hover:bg-green-500 transition-colors"
