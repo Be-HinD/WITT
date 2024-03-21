92e38f16c9e20813f052a9c556c6fc51be1dfbd5
이원주 <dldnjswn4265@gmail.com>
2024-03-21 15:35:44 +0900

    [FE] feat #S10P22D103-95: 404 Error 페이지 제작

--- frontend/src/pages/Error/index.tsx
+++ frontend/src/pages/Error/index.tsx
@@ -1,5 +1,19 @@
+import { RiProhibitedLine } from 'react-icons/ri'
+import { Link } from 'react-router-dom'
+
 const Error = () => {
-	return <div className="bg-black">404 page</div>
+	return (
+		<div className="text-white h-screen flex flex-col justify-center items-center gap-3">
+			<h1 className="flex justify-center items-center">
+				<RiProhibitedLine className="size-16" />
+				<p className="text-5xl font-bold">404</p>
+			</h1>
+			<p className="text-lg">페이지를 찾을 수 없습니다 :(</p>
+			<Link to="/" className="hover:text-textWthie hover:underline">
+				돌아가기
+			</Link>
+		</div>
+	)
 }
 
 export default Error
