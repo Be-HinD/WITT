2667080d847f2bd88080cdeb3bda21b80662875b
JIYEON KIM <jiyeon2536@naver.com>
2024-03-22 14:45:18 +0900

    [FE] feat  #S10P22D103-107 퀴즈 풀기 페이지 UI

--- frontend/src/pages/Quiz/index.tsx
+++ frontend/src/pages/Quiz/index.tsx
@@ -13,28 +13,41 @@ const Quiz = () => {
 			setCapturedImage(file)
 		}
 	}
+
 	const navigate = useNavigate()
 	const menu: IMenu = { left: icons.BACK, center: '퀴즈', right: undefined }
 	const func: IMenuFunc = { left_func: () => navigate('/'), right_func: undefined }
 	return (
 		<div>
 			<Header menu={menu} func={func}></Header>
-			<body>
-				<p className="pt-20">
+			<div>
+				<div className="pt-20 flex flex-col items-center justify-center ">
 					{capturedImage ? (
-						<img src={URL.createObjectURL(capturedImage)} />
+						<img src={URL.createObjectURL(capturedImage)} className="my-4" />
 					) : (
 						<div>
 							<HowToUse />
 						</div>
 					)}
-				</p>
-				<label
-					htmlFor="camera"
-					className="text-white font-semibold text-lg bg-purple-800 rounded-full px-6 py-2 leading-9 hover:bg-purple-500 transition-colors self-center mb-4"
-				>
-					사진찍고 퀴즈 풀어보기
-				</label>
+				</div>
+				{capturedImage ? (
+					<div className="flex justify-center">
+						<label htmlFor="camera">
+							<div className="text-white font-semibold text-lg bg-purple-800 rounded-full px-6 py-2 leading-9 hover:bg-purple-500 transition-colors mb-4 mr-3">
+								다시 찍기
+							</div>
+						</label>
+						<div className="text-white font-semibold text-lg bg-purple-800 rounded-full px-6 py-2 leading-9 hover:bg-purple-500 transition-colors mb-4">
+							제출하고 퀴즈 풀기
+						</div>
+					</div>
+				) : (
+					<label htmlFor="camera" className="flex justify-center">
+						<div className="text-white font-semibold text-lg bg-purple-800 rounded-full px-6 py-2 leading-9 hover:bg-purple-500 transition-colors mb-4 ">
+							사진 찍고 퀴즈 풀어보기
+						</div>
+					</label>
+				)}
 				<input
 					id="camera"
 					type="file"
@@ -43,7 +56,7 @@ const Quiz = () => {
 					onChange={handleCaptureImage}
 					className="hidden"
 				/>
-			</body>
+			</div>
 		</div>
 	)
 }
