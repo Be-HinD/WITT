de44dd26a09a88e2501478baaec592a2d46553d6
Be-HinD <kr100475@gmail.com>
2024-03-25 15:45:01 +0900

    fix #S10P22D103-53: 유저검색 API 무한참조 오류 수정 > Entity 반환하는 부분 ModelMapper활용하여 Dto 변환하도록 수정

--- backend/src/main/java/com/ssafy/rasingdust/domain/user/service/UserService.java
+++ backend/src/main/java/com/ssafy/rasingdust/domain/user/service/UserService.java
@@ -1,15 +1,15 @@
 package com.ssafy.rasingdust.domain.user.service;
 
+import com.ssafy.rasingdust.domain.user.dto.UserDto;
 import com.ssafy.rasingdust.domain.user.dto.request.AddUserRequest;
 import com.ssafy.rasingdust.domain.user.dto.response.FeedCharacterResponse;
-import com.ssafy.rasingdust.domain.user.dto.response.FollowResponse;
 import com.ssafy.rasingdust.domain.user.entity.User;
 import java.util.List;
 
 public interface UserService {
     public User findById(Long userId);
     public User findByUserName(String name);
-    public List<User> findByuserNameStartsWith(String userName);
+    public List<UserDto> findByuserNameStartsWith(String userName);
     public Long save(AddUserRequest addUserRequestDto);
     public void followUser(Long toId, Long fromId);
     public void unFollowUser(Long toId, Long fromId);
