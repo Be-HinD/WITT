47ee9f53b3505e8fb30743ac8436e26cf2cf630c
이원주 <dldnjswn4265@gmail.com>
2024-03-27 14:24:19 +0900

    [FE] feat #S10P22D103-135: 유저 검색 페이지 컴포넌트- 유저 검색 결과 컴포넌트 제작

--- frontend/src/pages/Search/index.tsx
+++ frontend/src/pages/Search/index.tsx
@@ -4,24 +4,30 @@ import { IMenu, IMenuFunc } from '../../components/interfaces'
 import { icons } from '../../constants/header-icons'
 import { useRouter } from '../../hooks/useRouter'
 import SearchBar from './components/SearchBar'
+import SearchItem from './components/SearchItem'
 
 const Search = () => {
 	const { goBack } = useRouter()
 	const menu: IMenu = { left: icons.BACK, center: '검색', right: undefined }
 	const func: IMenuFunc = { left_func: () => goBack(), right_func: undefined }
 
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    setInput(e.target.value)
-  }
+	const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		e.preventDefault()
+		setInput(e.target.value)
+	}
 
-  const [input, setInput] = useState('')
+	const [input, setInput] = useState('')
 	return (
-		<div className="text-textWthie">
+		<div className="text-whiteText">
 			<Header menu={menu} func={func} />
-			<div className="mt-14 px-4">
-				<SearchBar input={input} setInput={setInput} handleInput={handleInput}/>
-			</div>
+			<section className="flex flex-col gap-2 mt-14 px-4">
+				<SearchBar input={input} setInput={setInput} handleInput={handleInput} />
+				<fieldset>
+					<SearchItem />
+					<SearchItem />
+					<SearchItem />
+				</fieldset>
+			</section>
 		</div>
 	)
 }
