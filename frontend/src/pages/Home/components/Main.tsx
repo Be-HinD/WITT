44362d7f5c07def3f52ea7a305f8644263527507
김민우 <kmw7532@naver.com>
2024-03-27 15:32:05 +0900

    Replace Main.tsx

--- frontend/src/pages/Home/components/Main.tsx
+++ frontend/src/pages/Home/components/Main.tsx
@@ -3,7 +3,6 @@ import ActionBar from './ActionBar'
 import character from '../assets/tree.png'
 import background from '../assets/background_main.gif'
 import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 import { userstate } from '../../../components/StateVariables'
 
 const mainStyleClass = 'z-10 w-full h-screen px-[14px] py-[22px] bg-[#111111]'
@@ -11,10 +10,10 @@ const mainStyleClass = 'z-10 w-full h-screen px-[14px] py-[22px] bg-[#111111]'
 const noticeStyleClass = {
 	zIndex: '20',
 	position: 'absolute',
-	width: '80%',
+	width: '60%',
 	height: '60px',
-	top: '55%',
-	left: '10%',
+	top: '20%',
+	left: '15%',
 	padding: '20px 30px',
 	backgroundColor: '#1cd44d',
 	borderRadius: '50px',
@@ -28,46 +27,57 @@ const noticeStyleClass = {
 	transform: 'scaleX(0)',
 } as React.CSSProperties
 
-const buttonStyleClass = `
-flex justify-center items-center 
-z-[5] 
-w-[9.375rem] h-[3.125rem] 
-rounded-[15px]
-bg-[#3E3E3E] 
-text-[#ffffff]
-text-base 
-font-semibold
-border border-[#ffffff] 
-hover:scale-110 
-hover:bg-[#CE0CE0] 
-hover:transition-all 
-hover:cursor-pointer 
-transition ease-in-out duration-200 
-`
+// const buttonStyleClass = `
+// flex justify-center items-center
+// z-[5]
+// w-[9.375rem] h-[3.125rem]
+// rounded-[15px]
+// bg-[#3E3E3E]
+// text-[#ffffff]
+// text-base
+// font-semibold
+// border border-[#ffffff]
+// hover:scale-110
+// hover:bg-[#CE0CE0]
+// hover:transition-all
+// hover:cursor-pointer
+// transition ease-in-out duration-200
+// `
 
 const Main = () => {
-	const navigate = useNavigate()
 	const [noticeEffect, setNoticeEffect] = useState(noticeStyleClass)
 
-	const notices = userstate((state) => state.notices)
+	// const notices = userstate((state) => state.notices)
+	// const setNotices = userstate((state) => state.setNotices)
+	const newNotice = userstate((state) => state.newNotice)
+	const setNewNotice = userstate((state) => state.setNewNotice)
 	const isNewNotice = userstate((state) => state.isNewNotice)
 	const setIsNewNotice = userstate((state) => state.setIsNewNotice)
 
-	useEffect(() => {
+	const notify = () => {
 		setIsNewNotice()
+		console.log(isNewNotice)
 		if (isNewNotice) {
-			notices?.map(() => {})
+			setTimeout(() => {
+				setNoticeEffect({ ...noticeEffect, transform: 'scaleX(1)', transition: 'all 0.2s ease-out' })
+			}, 500)
+			setTimeout(() => {
+				setNoticeEffect({ ...noticeEffect, transform: 'scaleX(0)', transition: 'all 0.2s ease-out' })
+			}, 3500)
 		}
-		const starttimer = setTimeout(() => {
-			setNoticeEffect({ ...noticeEffect, transform: 'scaleX(1)', transition: 'all 0.2s ease-out' })
-		}, 500)
-		const endtimer = setTimeout(() => {
-			setNoticeEffect({ ...noticeEffect, transform: 'scaleX(0)', transition: 'all 0.2s ease-out' })
-		}, 3500)
-		return () => {
-			clearTimeout(starttimer)
-			clearTimeout(endtimer)
-		}
+	}
+
+	const getMyStatus = (state: string) => {
+		setNewNotice(`현재 캐릭터의 레벨은 ${state}입니다. 물을 주어 캐릭터를 성장시키세요!`)
+		// if (isNewNotice) {
+		// 	notices?.map(() => {})
+		// }
+		notify()
+	}
+
+	useEffect(() => {
+		setNewNotice('여기 새로운 알림 소식이 있습니다.....................')
+		notify()
 	}, [])
 
 	return (
@@ -77,40 +87,20 @@ const Main = () => {
 				style={{ position: 'absolute', opacity: '0.3' }}
 				src={background}
 			/>
-			<div
-				style={noticeEffect}
-				onClick={() => {
-					navigate('/follow')
-				}}
-			>
-				{`여기에 알림이 표시되었다 사라집니다.여기에 알림이 표시되었다 사라집니다.여기에 알림이 표시되었다
-				사라집니다.여기에 알림이 표시되었다 사라집니다.`}
-			</div>
+			<div style={noticeEffect}>{newNotice}</div>
 			<div
 				style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }}
 				className={mainStyleClass}
 			>
 				<UserProfile />
 				<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-					<img style={{ filter: 'brightness(1.3)' }} src={character} />
-				</div>
-				<div style={{ width: '100%', display: 'flex', justifyContent: 'space-around', transform: 'translateY(100%)' }}>
-					<div
-						className={buttonStyleClass}
+					<img
+						style={{ filter: 'brightness(1.3)', cursor: 'pointer' }}
+						src={character}
 						onClick={() => {
-							navigate('/quiz')
+							getMyStatus(`${'나무'}`)
 						}}
-					>
-						{'퀴즈 풀러 가기'}
-					</div>
-					<div
-						className={buttonStyleClass}
-						onClick={() => {
-							navigate('/follow')
-						}}
-					>
-						{'콕 찌르러 가기'}
-					</div>
+					/>
 				</div>
 				<ActionBar />
 			</div>
