fccfdd0a57fa8f00bc6901073909b20967d15571
newtkdtn <newtkdtn@gmail.com>
2024-03-28 17:02:51 +0900

    [BE] feat #S10P22D103-170: addBottle API

--- backend/src/main/java/com/ssafy/rasingdust/domain/problem/dto/response/AddBottleResponse.java
+++ backend/src/main/java/com/ssafy/rasingdust/domain/problem/dto/response/AddBottleResponse.java
@@ -1,18 +1,17 @@
-package com.ssafy.rasingdust.domain.user.dto.response;
+package com.ssafy.rasingdust.domain.problem.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @Builder
-public class FeedCharacterResponse {
-    // 물병, 성장 포인트
+public class AddBottleResponse {
     private int bottle;
-    private int growthPoint;
+    private int solvedCnt;
     private int rank;
+
 }
