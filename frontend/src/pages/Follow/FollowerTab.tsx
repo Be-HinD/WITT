09febf1361d140cdc405643a9474d3b29f49275c
이원주 <dldnjswn4265@gmail.com>
2024-03-29 16:21:48 +0900

    [FE] feat #S10P22D103-135: 팔로우 상세 페이지 이미지 경로 수정

--- frontend/src/pages/Follow/FollowerTab.tsx
+++ frontend/src/pages/Follow/FollowerTab.tsx
@@ -1,12 +1,11 @@
+import { dummy } from '../../constants/dummy-data'
 import FollowUser from './components/FollowUser'
 
-export const dummy = new Array(15).fill({})
 const FollowerTab = () => {
 	return (
 		<div className="pt-12 px-5">
-			{/* TODO: 실제 데이터 넣기, key값 설정 */}
-			{dummy.map(() => (
-				<FollowUser />
+			{dummy.map((user) => (
+				<FollowUser user={user}/>
 			))}
 		</div>
 	)
--- frontend/src/pages/Follow/FollowingTab.tsx
+++ frontend/src/pages/Follow/FollowingTab.tsx
@@ -1,11 +1,11 @@
+import { dummy } from '../../constants/dummy-data'
 import FollowUser from './components/FollowUser'
-import { dummy } from './FollowerTab'
 
 const FollowingTab = () => {
 	return (
 		<div className="pt-12 px-5">
-			{dummy.map(() => (
-				<FollowUser />
+			{dummy.map((user) => (
+				<FollowUser user={user}/>
 			))}
 		</div>
 	)
