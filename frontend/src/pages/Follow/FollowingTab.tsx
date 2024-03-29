62b8865eb57b6f3f9104c00e19984bec8550cb47
이원주 <dldnjswn4265@gmail.com>
2024-03-29 16:55:06 +0900

    [FE] feat #S10P22D103-163: 팔로우 탭 레이아웃 수정

--- frontend/src/pages/Follow/FollowingTab.tsx
+++ frontend/src/pages/Follow/FollowingTab.tsx
@@ -1,12 +1,18 @@
 import { dummy } from '../../constants/dummy-data'
+import FollowSkeleton from './components/FollowSkeleton'
 import FollowUser from './components/FollowUser'
 
 const FollowingTab = () => {
 	return (
 		<div className="pt-12 px-5">
-			{dummy.map((user) => (
-				<FollowUser user={user}/>
-			))}
+			<ul className="flex flex-col">
+				{dummy.map((user) => (
+					<li className="flex text-textWthi justify-between items-center gap-4 my-2">
+						<FollowUser user={user} />
+					</li>
+				))}
+				<FollowSkeleton />
+			</ul>
 		</div>
 	)
 }
