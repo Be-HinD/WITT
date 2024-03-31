fc1fbe1bf0856ad9cab98fc8ed8a8b32a735b3a2
김민우 <kmw7532@naver.com>
2024-03-31 11:43:32 +0900

    Update index.tsx

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -27,12 +27,6 @@ const Home = () => {
 			setIsLogin(true)
 		})
 		}
-		// const userToken = accessToken('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vcmFpc2luZ2R1c3Qvb2lqYWZkLmNvbSIsImlhdCI6MTcxMTg0MDc4OSwiZXhwIjoxNzEzMDUwMzg5LCJzdWIiOiIxMSIsImlkIjoxMX0.UkFIR3WTFiNsaRNeMCNxfFUjHxfYBykroI1IUbaSm50')
-		// userToken.then(async (value) => {
-			// localStorage.setItem('token', value)
-			// setIsLogin(true)
-		// })
-
 	}, [])
 
 	return <div>{isLogin ? <Main /> : <Splash />}</div>
