d0c5197734f3b38695e55b656bc62a444550bc13
김민우 <kmw7532@naver.com>
2024-04-01 12:58:13 +0900

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { CookiesProvider } from 'react-cookie'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
+export default function Root() {
+	return (
 		<CookiesProvider defaultSetOptions={{ path: '/' }}>
 			<App />
 		</CookiesProvider>
-	</React.StrictMode>
-)
+	)
+}
