de4294670038ed7a981472f60c1044689bf624ae
newtkdtn <newtkdtn@gmail.com>
2024-04-01 17:09:18 +0900

    HOTFIX : fix Response TokenControllerDocs

--- backend/src/main/java/com/ssafy/rasingdust/domain/jwt/token/controller/TokenControllerDocs.java
+++ backend/src/main/java/com/ssafy/rasingdust/domain/jwt/token/controller/TokenControllerDocs.java
@@ -16,7 +16,7 @@ public interface TokenControllerDocs {
     @Operation(summary = "AccessToken 생성 요청", description = "기존 AccessToken 만료된 경우 RequestBody로 RefreshToken을 받아 AccessToken 생성")
     @ApiResponse(responseCode = "201", description = "AccessToken 발급에 성공하였습니다.")
     @PostMapping("/token")
-    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken(@RequestBody CreateAccessTokenRequest request);
+    public ResponseEntity<ResultResponse> createNewAccessToken(@RequestBody CreateAccessTokenRequest request);
 
     @Operation(summary = "Develop Token 생성 요청", description = "PathVariable 받은 user_id에 해당하는 개발용 Token 생성후 반환")
     @ApiResponse(responseCode = "201", description = "개발용 토큰 발급에 성공하였습니다.")
