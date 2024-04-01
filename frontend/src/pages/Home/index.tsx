085a67b4ad4b4b5daf9fb1ac86582d2143d908e4
김민우 <kmw7532@naver.com>
2024-04-01 10:54:38 +0900

    fix: 로그인 로직 수정

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -2,29 +2,19 @@ import { useEffect } from 'react'
 import { mainstate } from '../../components/StateVariables'
 import Main from './components/Main'
 import Splash from './components/Splash'
-import { getToken, getUserData } from './components/API'
+// import { getToken, getUserData } from './components/API'
 import { Cookies } from 'react-cookie'
 
 const Home = () => {
 	const cookie = new Cookies()
-	const token = mainstate((state) => state.token)
 	const { isLogin, setIsLogin } = mainstate((state) => ({
 		isLogin: state.isLogin,
 		setIsLogin: state.setIsLogin,
 	}))
 
-	const getData = async () => {
-		if (cookie.get('refresh_token')) {
-			const accesstoken = await getToken(cookie.get('refresh_token')!)
-			localStorage.setItem('token', accesstoken)
-			const result = await getUserData(accesstoken!)
-			localStorage.setItem('mydata', JSON.stringify(result.data))
-		}
-	}
-
 	useEffect(() => {
-		if (token) {
-			getData().then(() => setIsLogin(true))
+		if (cookie.get('refresh_token')) {
+			setIsLogin(true)
 		}
 	}, [])
 
