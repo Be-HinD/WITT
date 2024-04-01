859adb2d3bac8e544979b965e3649bfcc6b564fa
김정현 <kr100475@gmail.com>
2024-04-01 14:38:10 +0900

    [BE] hotfix : import 추가

--- backend/src/main/java/com/ssafy/rasingdust/global/config/security/jwt/TokenAuthenticationFilter.java
+++ backend/src/main/java/com/ssafy/rasingdust/global/config/security/jwt/TokenAuthenticationFilter.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
 @Slf4j
