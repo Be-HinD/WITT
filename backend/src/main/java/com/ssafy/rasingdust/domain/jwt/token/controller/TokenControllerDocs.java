4cb6928e261920d7db27ece5c9026730fe1cbd1d
newtkdtn <newtkdtn@gmail.com>
2024-04-01 20:22:30 +0900

    [BE] fix #S10P22D103-191: fix DevelopTokenController

--- backend/src/main/java/com/ssafy/rasingdust/domain/jwt/token/controller/TokenControllerDocs.java
+++ backend/src/main/java/com/ssafy/rasingdust/domain/jwt/token/controller/TokenControllerDocs.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +21,7 @@ public interface TokenControllerDocs {
 
     @Operation(summary = "Develop Token 생성 요청", description = "PathVariable 받은 user_id에 해당하는 개발용 Token 생성후 반환")
     @ApiResponse(responseCode = "201", description = "개발용 토큰 발급에 성공하였습니다.")
-    @PostMapping("/token/develop/{user_id}")
-    public ResponseEntity<ResultResponse> createDevelopToken(@RequestParam Long userId);
+    @PostMapping("/token/develop/{userId}")
+    public ResponseEntity<ResultResponse> createDevelopToken(@PathVariable Long userId);
 
 }
