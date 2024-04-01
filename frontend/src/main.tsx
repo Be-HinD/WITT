ce95fd4bda5859b262c6cdf12e376215f5cab033
이원주 <dldnjswn4265@gmail.com>
2024-04-01 13:58:31 +0900

    [FE] hotfix: 빌드 에러 수정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,15 @@
 import App from './App.tsx'
+import ReactDOM from 'react-dom'
 import './index.css'
 import { CookiesProvider } from 'react-cookie'
+import React from 'react'
 
-export default function Root() {
-	return (
+ReactDOM.render(
+	<React.StrictMode>
+		{/* @ts-expect-error build */}
 		<CookiesProvider defaultSetOptions={{ path: '/' }}>
 			<App />
 		</CookiesProvider>
-	)
-}
+	</React.StrictMode>,
+	document.getElementById('root')
+)
