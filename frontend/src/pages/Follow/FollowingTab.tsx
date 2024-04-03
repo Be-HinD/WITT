2930c0382f0f954a0fd867bb4f47f893be9a6193
이원주 <dldnjswn4265@gmail.com>
2024-04-03 14:52:48 +0900

    [FE] feat #S10P22D103-143: 사용자 id 삽입

--- frontend/src/pages/Follow/FollowingTab.tsx
+++ frontend/src/pages/Follow/FollowingTab.tsx
@@ -2,12 +2,23 @@ import { useQuery } from '@tanstack/react-query'
 import { getFollowingList } from './api'
 import FollowUser from './components/FollowUser'
 import { IFollowUser } from './FollowerTab'
+import { useEffect, useState } from 'react'
 
 const FollowingTab = () => {
-	const userId = 103
+	const userInfo = localStorage.getItem('mydata')
+	const [userId, setUserId] = useState<number | null>(null)
+
+	useEffect(() => {
+		if (userInfo) {
+			const userData = JSON.parse(userInfo)
+			setUserId(userData.id)
+		}
+	})
+
 	const { data: following } = useQuery<IFollowUser[]>({
 		queryKey: ['following', userId],
 		queryFn: getFollowingList,
+		enabled: !!userId,
 	})
 
 	return (
