dbc6f2b22ba32f8321594512932d6b0a66ae98ce
이원주 <dldnjswn4265@gmail.com>
2024-04-03 15:58:21 +0900

    [FE] feat #S10P22D103-143: PWA 설치

--- frontend/src/pages/Error/index.tsx
+++ frontend/src/pages/Error/index.tsx
@@ -9,8 +9,7 @@ const Error = () => {
 				<p className="text-5xl font-bold">404</p>
 			</h1>
 			<p className="text-lg">페이지를 찾을 수 없습니다 :(</p>
-			<Link to="/search" className="hover:text-whiteText hover:underline">
-				{/* <Link to="/" className="hover:text-whiteText hover:underline"> */}
+			<Link to="/" className="hover:text-whiteText hover:underline">
 				돌아가기
 			</Link>
 		</div>
